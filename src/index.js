441db65af7894b9494b2c5fb6a30424958790801
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2023-03-05 06:25:46 -0300

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 /**!
- * @file fxhash boilerplate webpack  
- * @version 1.1.1.1  
+ * @file fxhash4  
+ * @version 0.0.1  
  * @copyright Iuri Guilherme 2023  
  * @license GNU AGPLv3  
  * @author Iuri Guilherme <https://iuri.neocities.org/>  
- * @author Laurent Houdard <https://github.com/laurent-h>  
- * @author ciphrd <https://github.com/ciphrd>  
- * @author fxhash <https://github.com/fxhash>  
+ * 
+ * Source code available at https://github.com/iuriguilherme/fxhash4
  * 
  * This program is free software: you can redistribute it and/or modify it 
  * under the terms of the GNU Affero General Public License as published by the 
@@ -23,9 +22,12 @@
  * 
  */
 
-import p5 from 'p5';
-import { create, all } from 'mathjs';
-const math = create(all, {})
+import { create as mcreate, all as mall } from "mathjs";
+const math = mcreate(mall, {})
+import Phaser from "phaser";
+//~ import Plotly from "plotly.js-dist-min";
+import Chart from "chart.js/auto";
+//~ import p5 from 'p5';
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 // https://github.com/fxhash/fxhash-webpack-boilerplate/issues/20
@@ -33,55 +35,132 @@ const properAlphabet =
     "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 const variantFactor = 3.904e-87; // This number is magic
 const fxhashDecimal = base58toDecimal(fxhashTrunc);
-const featureVariant = fxHashToVariant(fxhashDecimal, 1);
-//~ const featureVariant = -1;
-let size, scale, ratio, reWidth, reHeight;
-let width = window.innerWidth;
-let height = window.innerHeight;
+const featureVariant = math.max(1, fxHashToVariant(fxhashDecimal, 30));
+console.log("featureVariant: " + featureVariant);
+const startingSubjects = featureVariant;
 
-let sketch = function(p5) {
-  p5.setup = function() {
-    p5.randomSeed(fxrand() * 1e8);
-    p5.colorMode(p5.HSL);
-    ratio = width / height;
-    checkRatio();
-    size = p5.min(reWidth, reHeight);
-    p5.createCanvas(size, size);
-    scale = width / reWidth;
-    p5.scale(scale);
-    p5.frameRate(60);
-    p5.noLoop();
-  };
-  p5.draw = async function() {
-    p5.background(255);
-    console.log({
-      "fxhash": fxhashTrunc,
-      "fxhashDecimal": fxhashDecimal,
-      "featureVariant": featureVariant
-    })
-    await sleep(1);
-    fxpreview();
-  };
-  p5.windowResized = function() {
-    checkRatio();
-    size = p5.min(reWidth, reHeight);
-    p5.resizeCanvas(size, size);
-  }
-}
-let myp5 = new p5(sketch, window.document.body);
+const wrapperDiv = document.createElement("div");
+const graphDiv = document.createElement("div");
+const gameDiv = document.createElement("div");
+const graphCanvas = document.createElement("canvas");
+const gameCanvas = document.createElement("canvas");
+wrapperDiv.id = "wrapper";
+graphDiv.id = "graph";
+gameDiv.id = "game";
+graphCanvas.id = "graphCanvas";
+gameCanvas.id = "gameCanvas";
+document.body.appendChild(wrapperDiv);
+wrapperDiv.appendChild(graphDiv);
+wrapperDiv.appendChild(gameDiv);
+graphDiv.appendChild(graphCanvas);
+gameDiv.appendChild(gameCanvas);
 
-function checkRatio() {
-    let reRatio = window.innerWidth / window.innerHeight;
-    if (reRatio > ratio) {
-        scale = window.innerHeight / height;
-        reWidth = (window.innerHeight / height) * width;
-        reHeight = window.innerHeigth;
-    } else {
-        scale = window.innerWidth / width;
-        reWidth = window.innerWidth;
-        reHeight = (window.innerWidth / width) * height;
+class Example extends Phaser.Scene {
+    constructor () {
+        super();
+    }
+    
+    preload () {
+        this.load.svg("subject", "boy.svg");
+        this.load.svg("food", "heart.svg");
+        //~ const blob = new Blob([svgString], { type: 'image/svg+xml' });
+        //~ const url = URL.createObjectURL(blob);
+        //~ this.load.spritesheet(
+            //~ "dude",
+            //~ "dude.png",
+            //~ {
+                //~ "frameWidth": 178,
+                //~ "frameHeight": 198
+            //~ }
+        //~ );
+    }
+    
+    create () {
+        //~ let subjects = [];
+        let foods = [];
+        for (let i = 0; i < startingSubjects; i++) {
+            foods.push(this.add.image(0, 0, "food"));
+        }
+        const circle = new Phaser.Geom.Circle(
+            this.cameras.main.centerX,
+            this.cameras.main.centerY,
+            (this.cameras.main.width / 3)
+        );
+        this.group = this.add.group({
+            key: "subject",
+            //~ frame: [0, 1, 5],
+            repeat: startingSubjects
+        });
+        Phaser.Actions.PlaceOnCircle(this.group.getChildren(), circle);
+        Phaser.Actions.RandomCircle(foods, circle);
+        //~ var trace1 = {
+          //~ x: [1, 2, 3, 4],
+          //~ y: [10, 15, 13, 17],
+          //~ type: 'scatter'
+        //~ };
+        //~ var trace2 = {
+          //~ x: [1, 2, 3, 4],
+          //~ y: [16, 5, 11, 9],
+          //~ type: 'scatter'
+        //~ };
+        //~ var data = [trace1, trace2];
+        //~ let plot = Plotly.newPlot("graph", data);
+        let chart = new Chart(graphCanvas, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+    update () {
+        //~ Phaser.Actions.RotateAroundDistance(
+            //~ this.group.getChildren(),
+            //~ { x: 400, y: 300 },
+            //~ 0.02,
+            //~ this.tween.getValue()
+        //~ );
     }
 }
+
+const config = {
+    "type": Phaser.CANVAS,
+    "width": window.innerWidth - 
+        document.getElementById("graph").offsetWidth - 
+        (window.innerWidth / 60),
+    "height": window.innerHeight - 
+        document.getElementById("graph").offsetHeight - 
+        (window.innerHeight / 60),
+    "backgroundColor": "#e8e8e8",
+    //~ "canvas": document.getElementById("game"),
+    "canvas": gameCanvas,
+    "parent": "game",
+    //~ "physics": {
+        //~ "default": "arcade",
+        //~ "arcade": {
+            //~ "gravity": { y: 300 },
+            //~ "debug": false
+        //~ }
+    //~ },
+    "scale": {
+        "mode": Phaser.Scale.NONE,
+        "zoom": Phaser.Scale.MAX_ZOOM
+    },
+    "scene": Example
+};
+
+const game = new Phaser.Game(config);
 
 /**
  * @param {String} hash: unique fxhash string (or xtz transaction hash)
@@ -113,6 +192,11 @@ function fxHashToVariant(decimalHash, maxVariants = 0, inverse = false) {
     }
     return variant;
 }
+
+window.addEventListener(
+  "resize",
+  game.scale.setMaxZoom()
+);
 
 window.$fxhashFeatures = {
   "fx(variant)": featureVariant
